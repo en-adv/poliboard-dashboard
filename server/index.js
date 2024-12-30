51f4ab0f9a8dc6cdc8e0648f2603d70786cc9e53
Adventus Naibaho <78713237+en-adv@users.noreply.github.com>
2024-12-31 02:34:25 +0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import propertyRouter from './routes/property.routes.js';
 import metricRouter from './routes/metric.routes.js';
 import chartRouter from './routes/chart.routes.js';
 import datacalcRouter from './routes/datacalc.routes.js';
+import pricingRouter from './routes/pricing.routes.js';
 import './scheduler.js';
 
 dotenv.config();
@@ -25,6 +26,8 @@ app.use('/api/v1/properties', propertyRouter);
 app.use('/api/v1/metrics', metricRouter);
 app.use('/api/v1/charts', chartRouter);
 app.use('/api/v1/datacalc', datacalcRouter);
+app.use('/api/v1/pricing', pricingRouter);
+
 
 const startServer = async () => {
   try {
