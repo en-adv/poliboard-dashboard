cdaf3d67889a9ca0707f6a89b940ff7ec660e51b
Adventus Naibaho <78713237+en-adv@users.noreply.github.com>
2024-12-30 15:44:49 +0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -13,11 +13,7 @@ import './scheduler.js';
 dotenv.config();
 
 const app = express();
-app.use(cors({
-  origin: 'https://poliboard-dashboard-client.vercel.app',
-  methods: ['GET', 'POST'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-}));
+app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 
 app.get('/', (req, res) => {
